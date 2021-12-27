2248ca237d02355c08846c94118accb434c87081
KoooYoung <Jestinevallendradwiputra@gmail.com>
2021-12-27 20:32:52 +0700

    did some redone for the ingame ui and redone the level system. I also added a resume system to the game and made a fully working functions menu

--- script.js
+++ script.js
@@ -1,7 +1,84 @@
 
 
+
+
+
+// The Hitbox Algorithms
+let refreshSpeed = 1200;
+let stopRepeat = 0;
+let gameTime = 0;
+const levelUp = new Audio("./mp3/levelUp.mp3");
+const levelGame = document.querySelector(".level-game");
+const levelPopup = document.querySelector(".level-popup");
+let levelValue = 1;
+const scoreGame = document.querySelector(".score-game");
+const hitbox = document.querySelectorAll(".hitbox");
+const heart  = document.querySelectorAll(".fas");
+
+// animations 
+let doShake = true;
+let doZoomOut = true;
+function hitboxImg(){
+    if(inGame){
+        // generate random placement algorithm
+        let boxIndicator;
+        while(boxIndicator == stopRepeat){
+            boxIndicator = Math.floor(Math.random() * 9); 
+        }
+        stopRepeat = boxIndicator
+        
+        for (let i = 0; i <hitbox.length; i++) {
+            hitbox[i].style.animation="none"
+            if(i == boxIndicator){
+                hitbox[i].style.backgroundImage = "url('./svg/mole.svg')";
+                hitbox[i].classList.remove("flower")
+                hitbox[i].classList.add("mole")
+                if(doZoomOut) hitbox[i].style.backgroundSize= "60%";
+
+            }
+            else{
+                hitbox[i].style.backgroundImage = "url('./svg/flower.svg')";
+                hitbox[i].style.backgroundSize= "40%";
+                hitbox[i].classList.remove("mole")
+                hitbox[i].classList.add("flower")
+            }
+            if(refreshSpeed > 600 && doShake){
+                hitbox[i].style.animation=`resize-pic ${refreshSpeed}ms ease infinite`
+            }
+        }   
+    }
+}
+let refreshImage = setInterval(hitboxImg,refreshSpeed );
+// increase the speed
+setInterval(function(){
+    if(inGame){ 
+        gameTime++;
+        if(gameTime % 12 == 0){
+            if(refreshSpeed !=600){
+                refreshSpeed-= 100;
+            }
+            if(refreshSpeed > 600){
+                clearInterval(refreshImage);
+                levelUp.play();
+                levelUp.volume = 0.2;
+                levelGame.innerHTML = `LEVEL<br>${levelValue++}`;
+                levelPopup.classList.add("show-popup")
+                refreshImage = setInterval(hitboxImg,refreshSpeed);  
+                setTimeout(()=>{
+                    levelPopup.classList.remove("show-popup")
+                },800)
+            }
+        } 
+    }
+   
+},1000)    
+
+
+
+
 // Menu Event Listeners
 const menu = document.querySelector(".menu");
+const playBtn = document.querySelector(".play")
 const mainWindow = document.querySelector(".main-window");
 const gameWindow = document.querySelector(".game-window");
 const optionWindow = document.querySelector(".option-window");
@@ -9,16 +86,18 @@ const toOptions = document.querySelector(".options")
 const menuBack = document.querySelector(".menu-back");
 const optionBack = document.querySelector(".opt-back");
 let inGame = false;
+let onGoing = false;
 mainWindow.addEventListener("click", function(e) {
     if (e.target.classList.contains("play")){
         gameWindow.style.display = "grid";
         mainWindow.style.display = "none";
         inGame = true;
-        console.log(inGame)
+        onGoing = true
     }
 })
 menuBack.addEventListener("click", function() {
-    backToMenu()
+    turnToResumeBtn();
+    backToMenu();
 })
 toOptions.addEventListener("click", function(){
     optionWindow.style.display = "block";
@@ -38,79 +117,26 @@ function backToMenu(){
     }
 }
 
-
-
-// The Hitbox Algorithms
-let refreshSpeed = 1200;
-var stopRepeat = 0;
-let gameTime = 0;
-let bonus = false;
-const levelUp = new Audio("./mp3/levelUp.mp3")
-const hitbox = document.querySelectorAll(".hitbox");
-const scoreGame = document.querySelector(".score-game");
-const heart  = document.querySelectorAll(".fas")
-function hitboxImg(){
-    if(inGame){
-        // generate random placement algorithm
-        let boxIndicator;
-        while(boxIndicator == stopRepeat){
-            boxIndicator = Math.floor(Math.random() * 9); 
-        }
-        stopRepeat = boxIndicator
-        
-        for (let i = 0; i <hitbox.length; i++) {
-            hitbox[i].style.animation="none"
-            if(i == boxIndicator){
-                hitbox[i].style.backgroundImage = "url('./svg/mole.svg')";
-                hitbox[i].style.backgroundSize= "60%";
-                hitbox[i].classList.remove("flower")
-                hitbox[i].classList.add("mole")
-            }
-            else{
-                hitbox[i].style.backgroundImage = "url('./svg/flower.svg')";
-                hitbox[i].style.backgroundSize= "40%";
-                hitbox[i].classList.remove("mole")
-                hitbox[i].classList.add("flower")
-            }
-            if(refreshSpeed >= 600){
-                hitbox[i].style.animation=`resize-pic ${refreshSpeed}ms ease infinite`
-            }
-        }   
+function turnToResumeBtn(){
+    if(onGoing){
+        playBtn.textContent = "Resume"
     }
-}
-let refreshImage = setInterval(hitboxImg,refreshSpeed );
-// increase the speed
-setInterval(function(){
-    if(inGame){ 
-        gameTime++;
-        if(gameTime % 10 == 0){
-            if(refreshSpeed !=600){
-                refreshSpeed-= 100;
-            }
-            if(refreshSpeed >= 600){
-                clearInterval(refreshImage);
-                levelUp.play();
-                levelUp.volume = 0.2;
-                refreshImage = setInterval(hitboxImg,refreshSpeed);   
-            }
-        } 
+    else{
+        playBtn.textContent = "Play"
     }
-   
-},1000)    
-
-
+};
 
 
 
 // hitting the hitbox algorithm 
-const bonk = new Audio("./mp3/bonk.mp3")
-const wrong = new Audio("./mp3/wrong.mp3")
+const bonk = new Audio("./mp3/bonk.mp3");
+const wrong = new Audio("./mp3/wrong.mp3");
 let scoreValue = 1;
 let life = 3;
 gameWindow.addEventListener("click",function(e){
     if(e.target.classList.contains("hitbox")){
         if(e.target.classList.contains("mole")){
-            scoreGame.innerHTML = `SCORE: ${scoreValue++}`  
+            scoreGame.innerHTML = `SCORE<br>${scoreValue++}`  
             bonk.play();
             bonk.volume = 0.5;
         } 
@@ -128,13 +154,16 @@ gameWindow.addEventListener("click",function(e){
             heart[life].classList.replace("fas","far")
             alert("You Lost !");
             life = 3;
-            scoreValue =0;
+            levelValue= 1;
+            scoreValue =1;
             refreshSpeed = 1200;
-            scoreGame.innerHTML = `SCORE: 0`  
+            onGoing = false;
+            scoreGame.innerHTML = `SCORE<br>0`  
             for (let i = 0; i <heart.length; i++) {
                 heart[i].classList.replace("far","fas")
             }
             backToMenu()
+            turnToResumeBtn()
         }
 
         // Adding click animation
@@ -142,3 +171,22 @@ gameWindow.addEventListener("click",function(e){
     }
 })
 
+
+// Option Menu Selection
+const volumeOption = document.querySelector(".volume-opt");
+const shakingOption = document.querySelector(".shaking-opt");
+const zoomOption = document.querySelector(".zoom-opt");
+
+volumeOption.addEventListener("input",function(){
+    levelUp.volume = this.value - (this.value-0.2);
+    bonk.volume = this.value - (this.value-0.5);
+    wrong.volume = this.value - (this.value-0.05)
+})
+
+shakingOption.addEventListener("change",function(){
+    doShake = this.checked;
+})
+
+zoomOption.addEventListener("change",function(){
+    doZoomOut = this.checked;
+})
